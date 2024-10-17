ab1b51f2f87d71ef1ea61797b9c8a1a4111b96aa
varungiridhar <varungiridhar21@gmail.com>
2024-10-17 00:19:51 -0400

    RSSM block WIP

--- networks.py
+++ networks.py
@@ -288,6 +288,318 @@ class RSSM(nn.Module):
         loss = dyn_scale * dyn_loss + rep_scale * rep_loss
 
         return loss, value, dyn_loss, rep_loss
+    
+class RSSM_action_separate_with_group(nn.Module):
+    def __init__(
+        self,
+        stoch=30,
+        deter=200,
+        hidden=200,
+        rec_depth=1,
+        discrete=False,
+        act="SiLU",
+        norm=True,
+        mean_act="none",
+        std_act="softplus",
+        min_std=0.1,
+        unimix_ratio=0.01,
+        initial="learned",
+        num_actions=None,
+        embed=None,
+        device=None,
+        action_group=None,
+    ):
+        super(RSSM, self).__init__()
+        self._stoch = stoch
+        self._deter = deter
+        self._hidden = hidden
+        self._min_std = min_std
+        self._rec_depth = rec_depth
+        self._discrete = discrete
+        act = getattr(torch.nn, act)
+        self._mean_act = mean_act
+        self._std_act = std_act
+        self._unimix_ratio = unimix_ratio
+        self._initial = initial
+        self._num_actions = num_actions
+        self._embed = embed
+        self._device = device
+
+        # define the action group
+        assert action_group is not None
+        self._action_group = action_group
+        self.action_group_num = len(action_group)
+        print("the environment and task is set in {} action groups".format(self.action_group_num)) # move to better location
+        self.tile_target = (1, self.action_group_num, 1)
+
+        inp_layers = []
+        if self._discrete:
+            inp_dim = self._stoch * self._discrete + num_actions
+        else:
+            inp_dim = self._stoch + num_actions
+        inp_layers.append(nn.Linear(inp_dim, self._hidden, bias=False))
+        if norm:
+            inp_layers.append(nn.LayerNorm(self._hidden, eps=1e-03))
+        inp_layers.append(act())
+        self._img_in_layers = nn.Sequential(*inp_layers)
+        self._img_in_layers.apply(tools.weight_init)
+        self._cell = GRUCell(self._hidden, self._deter, norm=norm)
+        self._cell.apply(tools.weight_init)
+
+        img_out_layers = []
+        inp_dim = self._deter
+        img_out_layers.append(nn.Linear(inp_dim, self._hidden, bias=False))
+        if norm:
+            img_out_layers.append(nn.LayerNorm(self._hidden, eps=1e-03))
+        img_out_layers.append(act())
+        self._img_out_layers = nn.Sequential(*img_out_layers)
+        self._img_out_layers.apply(tools.weight_init)
+
+        obs_out_layers = []
+        inp_dim = self._deter + self._embed
+        obs_out_layers.append(nn.Linear(inp_dim, self._hidden, bias=False))
+        if norm:
+            obs_out_layers.append(nn.LayerNorm(self._hidden, eps=1e-03))
+        obs_out_layers.append(act())
+        self._obs_out_layers = nn.Sequential(*obs_out_layers)
+        self._obs_out_layers.apply(tools.weight_init)
+
+        if self._discrete:
+            self._imgs_stat_layer = nn.Linear(
+                self._hidden, self._stoch * self._discrete
+            )
+            self._imgs_stat_layer.apply(tools.uniform_weight_init(1.0))
+            self._obs_stat_layer = nn.Linear(self._hidden, self._stoch * self._discrete)
+            self._obs_stat_layer.apply(tools.uniform_weight_init(1.0))
+        else:
+            self._imgs_stat_layer = nn.Linear(self._hidden, 2 * self._stoch)
+            self._imgs_stat_layer.apply(tools.uniform_weight_init(1.0))
+            self._obs_stat_layer = nn.Linear(self._hidden, 2 * self._stoch)
+            self._obs_stat_layer.apply(tools.uniform_weight_init(1.0))
+
+        if self._initial == "learned":
+            self.W = torch.nn.Parameter(
+                torch.zeros((1, self._deter), device=torch.device(self._device)),
+                requires_grad=True,
+            )
+
+    def initial(self, batch_size):
+        deter = torch.zeros(batch_size, self._deter, device=self._device)
+        if self._discrete:
+            state = dict(
+                logit=torch.zeros(
+                    [batch_size, self._stoch, self._discrete], device=self._device
+                ),
+                stoch=torch.zeros(
+                    [batch_size, self._stoch, self._discrete], device=self._device
+                ),
+                deter=deter,
+            )
+        else:
+            state = dict(
+                mean=torch.zeros([batch_size, self._stoch], device=self._device),
+                std=torch.zeros([batch_size, self._stoch], device=self._device),
+                stoch=torch.zeros([batch_size, self._stoch], device=self._device),
+                deter=deter, #TODO: this needs to be changes
+            )
+        if self._initial == "zeros":
+            return state
+        elif self._initial == "learned":
+            state["deter"] = torch.tanh(self.W).repeat(batch_size, 1)
+            state["stoch"] = self.get_stoch(state["deter"])
+            return state
+        else:
+            raise NotImplementedError(self._initial)
+
+    def observe(self, embed, action, is_first, state=None):
+        swap = lambda x: x.permute([1, 0] + list(range(2, len(x.shape))))
+        # (batch, time, ch) -> (time, batch, ch)
+        embed, action, is_first = swap(embed), swap(action), swap(is_first)
+        # prev_state[0] means selecting posterior of return(posterior, prior) from obs_step
+        post, prior = tools.static_scan(
+            lambda prev_state, prev_act, embed, is_first: self.obs_step(
+                prev_state[0], prev_act, embed, is_first
+            ),
+            (action, embed, is_first),
+            (state, state),
+        )
+
+        # (batch, time, stoch, discrete_num) -> (batch, time, stoch, discrete_num)
+        post = {k: swap(v) for k, v in post.items()}
+        prior = {k: swap(v) for k, v in prior.items()}
+        return post, prior
+
+    def imagine_with_action(self, action, state):
+        swap = lambda x: x.permute([1, 0] + list(range(2, len(x.shape))))
+        assert isinstance(state, dict), state
+        action = swap(action)
+        prior = tools.static_scan(self.img_step, [action], state)
+        prior = prior[0]
+        prior = {k: swap(v) for k, v in prior.items()}
+        return prior
+
+    def get_feat(self, state):
+        stoch = state["stoch"]
+        if self._discrete:
+            shape = list(stoch.shape[:-2]) + [self._stoch * self._discrete]
+            stoch = stoch.reshape(shape)
+        return torch.cat([stoch, state["deter"]], -1)
+
+    def get_dist(self, state, dtype=None):
+        if self._discrete:
+            logit = state["logit"]
+            dist = torchd.independent.Independent(
+                tools.OneHotDist(logit, unimix_ratio=self._unimix_ratio), 1
+            )
+        else:
+            mean, std = state["mean"], state["std"]
+            dist = tools.ContDist(
+                torchd.independent.Independent(torchd.normal.Normal(mean, std), 1)
+            )
+        return dist
+
+    def obs_step(self, prev_state, prev_action, embed, is_first, sample=True):
+        # initialize all prev_state
+        if prev_state == None or torch.sum(is_first) == len(is_first):
+            prev_state = self.initial(len(is_first))
+            prev_action = torch.zeros(
+                (len(is_first), self._num_actions), device=self._device
+            )
+        # overwrite the prev_state only where is_first=True
+        elif torch.sum(is_first) > 0:
+            is_first = is_first[:, None]
+            prev_action *= 1.0 - is_first
+            init_state = self.initial(len(is_first))
+            for key, val in prev_state.items():
+                is_first_r = torch.reshape(
+                    is_first,
+                    is_first.shape + (1,) * (len(val.shape) - len(is_first.shape)),
+                )
+                prev_state[key] = (
+                    val * (1.0 - is_first_r) + init_state[key] * is_first_r
+                )
+
+        prior = self.img_step(prev_state, prev_action)
+        x = torch.cat([prior["deter"], embed], -1)
+        # (batch_size, prior_deter + embed) -> (batch_size, hidden)
+        x = self._obs_out_layers(x)
+        # (batch_size, hidden) -> (batch_size, stoch, discrete_num)
+        stats = self._suff_stats_layer("obs", x)
+        if sample:
+            stoch = self.get_dist(stats).sample()
+        else:
+            stoch = self.get_dist(stats).mode()
+        post = {"stoch": stoch, "deter": prior["deter"], **stats}
+        return post, prior
+
+    def img_step(self, prev_state, prev_action, sample=True):
+        # (batch, stoch, discrete_num)
+        prev_stoch = prev_state["stoch"]
+        if self._discrete:
+            shape = list(prev_stoch.shape[:-2]) + [self._stoch * self._discrete]
+            # (batch, stoch, discrete_num) -> (batch, stoch * discrete_num)
+            prev_stoch = prev_stoch.reshape(shape)
+        # (batch, stoch * discrete_num) -> (batch, stoch * discrete_num + action)
+        x = torch.cat([prev_stoch, prev_action], -1)
+        
+        # formatting data according to ED2
+        action_split = torch.split(prev_action, 1, dim=-1)
+        x = x.repeat(self.tile_target)
+        x_a_b_s = x.permute(1, 0, 2)
+
+        # (batch, stoch * discrete_num + action, embed) -> (batch, hidden)
+        # x = self._img_in_layers(x)
+        sum_x = []
+        sum_deter = []
+        for i in range(self.action_group_num):
+            ed2_action_group = self.action_group[i]
+            action_tensor = torch.cat([self.action_split[_] for _ in ed2_action_group], -1)
+            s_a_pair = torch.cat([prev_state["deter"], action_tensor], -1)
+            tmp = self._img_in_layers(s_a_pair)
+            # tmp, deter_tmp = self._cell(tmp, [prev_state['deter']])
+            for _ in range(self._rec_depth):  # rec depth is not correctly implemented
+                deter_tmp = prev_state["deter"]
+                # (batch, hidden), (batch, deter) -> (batch, deter), (batch, deter)
+                tmp, deter_tmp = self._cell(tmp, [deter])
+                deter_tmp = deter_tmp[0]  # Keras wraps the state in a list.
+            sum_x.append(tmp)
+            sum_deter.append(deter_tmp[0])
+        # reduce to mean
+        x = torch.mean(torch.stack(sum_x, 0), 0)
+        deter = torch.mean(torch.stack(sum_deter, 0), 0)
+
+        # for _ in range(self._rec_depth):  # rec depth is not correctly implemented
+        #     deter = prev_state["deter"]
+        #     # (batch, hidden), (batch, deter) -> (batch, deter), (batch, deter)
+        #     x, deter = self._cell(x, [deter])
+        #     deter = deter[0]  # Keras wraps the state in a list.
+        # (batch, deter) -> (batch, hidden)
+        x = self._img_out_layers(x)
+        # (batch, hidden) -> (batch_size, stoch, discrete_num)
+        stats = self._suff_stats_layer("ims", x)
+        if sample:
+            stoch = self.get_dist(stats).sample()
+        else:
+            stoch = self.get_dist(stats).mode()
+        prior = {"stoch": stoch, "deter": deter, **stats}
+        return prior
+
+    def get_stoch(self, deter):
+        x = self._img_out_layers(deter)
+        stats = self._suff_stats_layer("ims", x)
+        dist = self.get_dist(stats)
+        return dist.mode()
+
+    def _suff_stats_layer(self, name, x):
+        if self._discrete:
+            if name == "ims":
+                x = self._imgs_stat_layer(x)
+            elif name == "obs":
+                x = self._obs_stat_layer(x)
+            else:
+                raise NotImplementedError
+            logit = x.reshape(list(x.shape[:-1]) + [self._stoch, self._discrete])
+            return {"logit": logit}
+        else:
+            if name == "ims":
+                x = self._imgs_stat_layer(x)
+            elif name == "obs":
+                x = self._obs_stat_layer(x)
+            else:
+                raise NotImplementedError
+            mean, std = torch.split(x, [self._stoch] * 2, -1)
+            mean = {
+                "none": lambda: mean,
+                "tanh5": lambda: 5.0 * torch.tanh(mean / 5.0),
+            }[self._mean_act]()
+            std = {
+                "softplus": lambda: torch.softplus(std),
+                "abs": lambda: torch.abs(std + 1),
+                "sigmoid": lambda: torch.sigmoid(std),
+                "sigmoid2": lambda: 2 * torch.sigmoid(std / 2),
+            }[self._std_act]()
+            std = std + self._min_std
+            return {"mean": mean, "std": std}
+
+    def kl_loss(self, post, prior, free, dyn_scale, rep_scale):
+        kld = torchd.kl.kl_divergence
+        dist = lambda x: self.get_dist(x)
+        sg = lambda x: {k: v.detach() for k, v in x.items()}
+
+        rep_loss = value = kld(
+            dist(post) if self._discrete else dist(post)._dist,
+            dist(sg(prior)) if self._discrete else dist(sg(prior))._dist,
+        )
+        dyn_loss = kld(
+            dist(sg(post)) if self._discrete else dist(sg(post))._dist,
+            dist(prior) if self._discrete else dist(prior)._dist,
+        )
+        # this is implemented using maximum at the original repo as the gradients are not backpropagated for the out of limits.
+        rep_loss = torch.clip(rep_loss, min=free)
+        dyn_loss = torch.clip(dyn_loss, min=free)
+        loss = dyn_scale * dyn_loss + rep_scale * rep_loss
+
+        return loss, value, dyn_loss, rep_loss
 
 
 class MultiEncoder(nn.Module):
